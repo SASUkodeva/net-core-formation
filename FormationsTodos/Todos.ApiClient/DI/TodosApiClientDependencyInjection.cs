b6bd3bbc020c16ffc997e15e6e530a7869c2ecd3
Yohann Tanguy <yohann.tanguy@kodeva.fr>
2023-07-02 09:34:36 +0200

    Add Swagger and .NET Api Client

---
+++ FormationsTodos/Todos.ApiClient/DI/TodosApiClientDependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todos.ApiClient.Models;
+
+namespace Todos.ApiClient.DI
+{
+    public static  class TodosApiClientDependencyInjection
+    {
+
+        public static IServiceCollection AddTodosApiClient(this IServiceCollection services, string baseUrl) {
+
+           
+                services.AddScoped<IClient, Client>();
+                services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri(baseUrl));
+                return services;
+           
+        
+        }
+    }
+}
